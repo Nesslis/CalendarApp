d76da4200844cee46db7e47711b8d9b90e0e1df6
Nesslis <112620849+Nesslis@users.noreply.github.com>
2024-07-31 15:15:00 +0300

    Update: Home
    Show daily events in homePage

--- app/screens/Home/home.tsx
+++ app/screens/Home/home.tsx
@@ -1,21 +1,61 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-
-const events = [
-  { time: '09:00 - 11:00', title: 'Toplantı Başlığı' },
-  { time: '14:00 - 15:00', title: 'Ziyaretçi Başlığı' },
-];
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, ActivityIndicator } from 'react-native';
+import moment from 'moment';
+import { useAuth } from '../../context/AuthContext';
+interface Event {
+  event_id: number;
+  user_id: number;
+  category_id: number;
+  title: string;
+  date: string;
+  time: string;
+  location: string;
+  participant: string | null;
+  content: string | null;
+}
 export default function HomePage() {
+  const { onSearchEvents } = useAuth();
+  const [events, setEvents] = useState<Event[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      try {
+        if(!onSearchEvents) return;
+        const today = moment().format('YYYY-MM-DD');
+        const fetchedEvents = await onSearchEvents(undefined, today, today); // Fetch events for today
+        setEvents(fetchedEvents);
+      } catch (error) {
+        console.error('Error fetching events:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchEvents();
+  }, [onSearchEvents]);
+
+  const formatTime = (time : string) => {
+    return moment(time, 'HH:mm:ss').format('HH:mm');
+  };
+
+  if (loading) {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator size="large" color="#03346E" />
+      </View>
+    );
+  }
 
   return (
     <View style={styles.container}>
-      <Text style={styles.eventTitle}>Bugünkü etkinlikleriniz:</Text>
+      <Text style={styles.headerText}>Bugünkü etkinlikleriniz:</Text>
       <View style={styles.separator} />
       {events.length > 0 ? (
         events.map((event, index) => (
           <View key={index} style={styles.eventBox}>
-            <Text style={styles.eventTime}>{event.time}</Text>
-            <Text style={styles.eventsTitle}>{event.title}</Text>
+            <Text style={styles.eventTime}>{formatTime(event.time)}</Text>
+            <Text style={styles.eventTitle}>{event.title}</Text>
           </View>
         ))
       ) : (
@@ -33,68 +73,50 @@ const styles = StyleSheet.create({
     alignItems: 'flex-start',
     padding: 20,
   },
-  welcomeBox: {
-    backgroundColor: '#FFFFFF',
-    alignItems:'flex-start',
-    padding: 20,
-    paddingRight: 45,
-    borderRadius: 20,
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5,
-    marginBottom: 20,
-  },
-  welcomeText: {
-    fontSize: 18,
-    color: '#333',
-  },
-  userName: {
-    fontSize: 22,
+  headerText: {
+    fontSize: 24,
     fontWeight: 'bold',
-    color: '#333',
-    marginTop: 5,
+    color: '#03346E',
+    marginBottom: 30,
+    marginTop: 20,
+    textAlign: 'left',
   },
   eventTitle: {
     fontSize: 18,
-    color: '#333',
+    color: '#03346E',
     marginVertical: 10,
-    alignItems:'center',
-    alignSelf:'center',
-    marginTop:85,
+    alignItems: 'center',
+    alignSelf: 'center',
+    marginTop: 15,
   },
   separator: {
     height: 1,
-    backgroundColor: '#CCC',
+    backgroundColor: '#7FA1C3',
     alignSelf: 'stretch',
     marginVertical: 10,
-    marginTop:25,
-    marginBottom:45,
+    marginTop: 25,
+    marginBottom: 55,
   },
   eventBox: {
-    backgroundColor: '#FFB6C1',
-    padding: 15,
+    backgroundColor: 'rgba(182, 199, 170, 0.5)',
+    padding: 10,
     borderRadius: 10,
     marginVertical: 5,
-    width: '90%',
-    alignSelf:'center',
-    marginBottom:15,
+    width: '80%',
+    alignSelf: 'center',
+    marginBottom: 25,
   },
   eventTime: {
     fontSize: 16,
     fontWeight: 'bold',
-    color: '#333',
+    color: '#03346E',
   },
   eventsTitle: {
     fontSize: 18,
-    color: '#333',
+    color: '#03346E',
     marginTop: 5,
-    alignItems:'center',
-    alignSelf:'center',
+    alignItems: 'center',
+    alignSelf: 'center',
   },
   noEvents: {
     fontSize: 16,
